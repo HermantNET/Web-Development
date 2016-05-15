04c04c9f87fae586a4fdbcd6aebac61be8f9c035
Thomas <hermant.edu@gmail.com>
2016-05-15 13:21:18 -0400

    finished dom manipulation demo

---
+++ Dom Manipulation/script.js
@@ -0,0 +1,5 @@
+var button = document.querySelector("input[type=button]");
+var h1 = document.querySelector("h1");
+
+button.addEventListener("click", function () {
+    h1.classList.toggle("alt");})
